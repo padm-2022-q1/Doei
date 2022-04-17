6920dffedc42df9bbca25f83513afcffcf9acec7
Lucas <lribeiro@vidalink.com.br>
2022-04-16 23:33:09 -0300

    Criada tela de meus interesses

---
+++ app/src/main/java/com/example/doei/ui/myInterests/MyInterestsFragment.kt
@@ -0,0 +1,35 @@
+package com.example.doei.ui.myInterests
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.doei.databinding.FragmentMyInterestsBinding
+
+class NotificationsFragment : Fragment() {
+
+    private var _binding: FragmentMyInterestsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        ViewModelProvider(this)[MyInterestsViewModel::class.java]
+
+        _binding = FragmentMyInterestsBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
