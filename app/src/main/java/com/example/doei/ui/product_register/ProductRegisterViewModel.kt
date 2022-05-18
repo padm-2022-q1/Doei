8c27aaa9515e6fd32745200e24e66da53ae5d21c
wilwilcosta <wil.acneto@gmail.com>
2022-05-18 16:14:27 -0300

    viewmodel com @AndroidEntrypoint

--- app/src/main/java/com/example/doei/ui/product_register/ProductRegisterViewModel.kt
+++ app/src/main/java/com/example/doei/ui/product_register/ProductRegisterViewModel.kt
@@ -3,10 +3,12 @@ package com.example.doei.ui.product_register
 import androidx.lifecycle.ViewModel
 import com.example.doei.domain.models.Product
 import com.example.doei.domain.repository.FirebaseDatabaseRepository
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
+@AndroidEntryPoint
 class ProductRegisterViewModel @Inject constructor(private val firebaseDatabaseRepository: FirebaseDatabaseRepository)  : ViewModel() {
     fun addProductToDatabase(product : Product) : Boolean{
         return firebaseDatabaseRepository.addProductToDatabase(product)
